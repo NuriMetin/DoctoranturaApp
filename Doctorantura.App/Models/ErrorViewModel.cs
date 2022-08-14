e11bf7289bcdeeda9c7a8b6d10e9fb8faadcb245
NuriMetin <nurimetin98@gmail.com>
2022-08-14 03:00:52 -0700

    first commit

---
+++ Doctorantura.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Doctorantura.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
